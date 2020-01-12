42cc7f4669cdcc2d9cff6f595fece5ffa473f78f
Jakub Míšek <jmisek@gmail.com>
2020-01-12 14:51:45 +0100

    rendering blocks, cleaned paths
    TwigBlock

--- Twig.AspNetCore/TwigHtmlHelperExtension.cs
+++ Twig.AspNetCore/TwigHtmlHelperExtension.cs
@@ -25,7 +25,9 @@ namespace Microsoft.AspNetCore.Mvc  // implicitly imported namespace in Razor pa
 
             public string Path { get; set; }
 
-            public string Name { get; set; }
+            public string Template { get; set; }
+
+            public string BlockName { get; set; }   // optional
 
             public PhpArray Data { get; set; }
 
@@ -36,29 +38,52 @@ namespace Microsoft.AspNetCore.Mvc  // implicitly imported namespace in Razor pa
 
             public void WriteTo(TextWriter writer, HtmlEncoder encoder)
             {
+                // Pchp.Library.Spl.Autoload.spl_autoload_register(_ctx, ... ) // TODO: autoload, needed when PHP scripts are included
+
                 var loader = new Twig.Loader.FilesystemLoader(_ctx, Path);
                 var twig = new Twig.Environment(_ctx, loader, PhpValue.Null);
 
-                var output = twig.render(Name, Data ?? PhpArray.Empty);
+                var template = twig.load(Template);
+
+                var output = BlockName == null
+                    ? template.render(Data)
+                    : template.renderBlock(BlockName, Data);
 
                 //
                 writer.Write(output);
             }
         }
-
-        /// <summary>
-        /// Returns output for the specified PHP script.
-        /// </summary>
-        public static IHtmlContent Twig(this IHtmlHelper htmlHelper, string path, string name, object model = null)
+        
+        static TwigContent MakeTwigContent(IHtmlHelper htmlHelper, string path, string template, object model = null)
         {
             return new TwigContent(
                 ctx: htmlHelper.ViewContext.HttpContext.GetOrCreateContext()    // PHP context for current HTTP context
             )
             {
                 Path = path,
-                Name = name,
+                Template = template,
                 Data = model as PhpArray ?? Pchp.Core.Convert.ToArray(model ?? htmlHelper.ViewData),
             };
+        }
+
+        /// <summary>
+        /// Returns output for the specified Twig template.
+        /// </summary>
+        public static IHtmlContent Twig(this IHtmlHelper htmlHelper, string path, string template, object model = null)
+        {
+            return MakeTwigContent(htmlHelper, path, template, model);
+        }
+
+        /// <summary>
+        /// Returns output for the specified Twig block within a template.
+        /// </summary>
+        public static IHtmlContent TwigBlock(this IHtmlHelper htmlHelper, string path, string template, string block, object model = null)
+        {
+            var content = MakeTwigContent(htmlHelper, path, template, model);
+
+            content.BlockName = block ?? throw new ArgumentNullException(nameof(block));
+
+            return content;
         }
 
         /// <summary>
